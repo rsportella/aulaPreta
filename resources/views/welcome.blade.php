5cf7162160d642ab17ab51bb10b78d900b3b6ab4
Portella <rodolfosportella@gmail.com>
2019-06-15 03:53:01 -0300

    inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Olá mundo denovo
+                    Olá mundo
                 </div>
             </div>
         </div>
