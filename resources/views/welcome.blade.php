3f10f149501f35218180aad6059f16117141f091
Portella <rodolfosportella@gmail.com>
2019-06-16 00:22:14 -0300

    Alteracao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Olá mundo, deploy por Jenkins
+                    Olá mundo
                 </div>
             </div>
         </div>
