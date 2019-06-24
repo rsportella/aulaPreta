3b8b2cf2ec85f5415d544d22dd6fb0c16106ecd5
Rodolfo Portella <rodolfosportella@gmail.com>
2019-06-23 23:30:55 -0300

    Aula

--- database/migrations/2019_06_22_080159_create_servicos_table.php
+++ database/migrations/2019_06_22_080159_create_servicos_table.php
@@ -35,6 +35,6 @@ class CreateServicosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('protocolos');
+        Schema::dropIfExists('servicos');
     }
 }
